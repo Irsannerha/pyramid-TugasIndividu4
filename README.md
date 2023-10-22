2c584bec403315fb27baa947e3c9e5b22949f892
Irsannerha <irsanrh275@gmail.com>
2023-10-22 21:33:00 +0700

    Pyramid TugasIndividu 4

---
+++ README.md
@@ -0,0 +1,13 @@
+# Pyramid Tugas Individu 4
+
+Nama : Irsan Romardi Harahap
+NIM : 120140043
+
+## Let's Get Started
+
+### 1. Clone Repository
+
+Clone repository ini ke komputer Anda:
+
+```bash
+git clone <repository URL>
---
+++ setup.py
@@ -0,0 +1,64 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'plaster_pastedeploy',
+    'pyramid',
+    'pyramid_jinja2',
+    'pyramid_debugtoolbar',
+    'PyJWT',
+    'pyramid_jwt',
+    'pymysql',
+    'waitress',
+    'alembic',
+    'pyramid_retry',
+    'pyramid_tm',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+]
+
+tests_require = [
+    'WebTest',
+    'pytest',
+    'pytest-cov',
+]
+
+setup(
+    name='pyramid_TugasIndividu4',
+    version='0.0',
+    description='pyramid Tugas Individu 4',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Pyramid',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    ],
+    author='',
+    author_email='',
+    url='',
+    keywords='web pyramid pylons',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    extras_require={
+        'testing': tests_require,
+    },
+    install_requires=requires,
+    entry_points={
+        'paste.app_factory': [
+            'main = pyramid_TugasIndividu4:main',
+        ],
+        'console_scripts': [
+            'initialize_pyramid_TugasIndividu4_db=pyramid_TugasIndividu4.scripts.initialize_db:main',
+        ],
+    },
+)
